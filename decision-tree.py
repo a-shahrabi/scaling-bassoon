7db26ec33d61edbcb3b515689e245143518d7f1f
a-shahrabi <ardavan.shahrabi@dal.ca>
2025-03-17 22:33:55 -0400

    pandas

--- decision-tree.py
+++ decision-tree.py
@@ -1,5 +1,5 @@
 import numpy as np
-# import pandas as pd
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
